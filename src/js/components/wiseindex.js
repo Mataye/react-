cf5a78c8dc1716e1355384accc905303fb2c734a
yehang <1335332078@qq.com>
2017-11-25 23:33:16 +0800

    增加轮播图

--- src/js/components/wiseindex.js
+++ src/js/components/wiseindex.js
@@ -4,17 +4,33 @@ import WiseFooter from "./wisefooter";
 import
 {
     Tabs,
+    Carousel
 } from "antd";
 import  WiseList from "./wiselist";
 const TabPane = Tabs.TabPane;
 
 export default class WiseIndex extends React.Component {
     render(){
+        const settings= {
+            dots:true,
+            infinite:true,
+            speed:500,
+            slidesToShow:1,
+            autoplay:true
+        };
         return(
             <div>
                 <WiseHeader></WiseHeader>
                 <Tabs>
                     <TabPane tab={"头条"} key="1">
+                        <div className="carousel">
+                            <Carousel  {...settings}>
+                                <div><img src="./src/images/carousel_1.jpg"/></div>
+                                <div><img src="./src/images/carousel_2.jpg"/></div>
+                                <div><img src="./src/images/carousel_3.jpg"/></div>
+                                <div><img src="./src/images/carousel_4.jpg"/></div>
+                            </Carousel>
+                        </div>
                         <WiseList count={20} type="top"/>
                     </TabPane>
                     <TabPane tab={"社会"} key="2">
--- src/js/components/wiselist.js
+++ src/js/components/wiselist.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Row,Col} from 'antd';
+
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 export default class WiseList extends React.Component {
     constructor() {
